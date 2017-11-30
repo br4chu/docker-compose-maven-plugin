4e52e97c4486ffd1b35ad7f08771bfdc767b48a0
Marcin Jaguś <marcin.jagus@protonmail.com>
2017-11-30 09:29:03 +0100

    First test for plugin's default configuration

--- test-harness/src/test/java/io/brachu/docker/compose/plugin/test/harness/DefaultsIT.java
+++ test-harness/src/test/java/io/brachu/docker/compose/plugin/test/harness/DefaultsIT.java
@@ -1,5 +1,14 @@
 package io.brachu.docker.compose.plugin.test.harness;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
 public class DefaultsIT {
+
+    @Test
+    public void shouldRunOnDefaults() {
+        assertThat(System.getProperty("maven.dockerCompose.project")).isNotNull();
+    }
 
 }
